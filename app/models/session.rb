f6691feb56f017998f4871b1aac6ab92a12c485e
frankfang <frankfang1990@gmail.com>
2020-06-23 03:52:05 +0800

    添加自定义校验，检查邮件是否注册

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,9 +3,18 @@ class Session
   attr_accessor :email, :password
 
   validates :email, presence: true
+  validate :check_email, if: :email
   validates :password, presence: true
 
   validates_format_of :email, with: /.+@.+/, if: :email
   validates :password, length: {minimum: 6}, if: :password
+
+
+  def check_email
+    user = User.find_by_email email
+    if user.nil?
+      errors.add :email, :not_found
+    end
+  end
 
 end
