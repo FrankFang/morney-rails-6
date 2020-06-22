fa361eb76b42252ef7101403d06aaa92047f32ec
frankfang <frankfang1990@gmail.com>
2020-06-23 04:26:51 +0800

    密码不匹配

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
     s = Session.new create_params
     s.validate
     render_resource s
+    # session[:current_user_id] = session.user.id
   end
   def destroy
   
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,6 @@
 class Session
   include ActiveModel::Model
-  attr_accessor :email, :password
+  attr_accessor :email, :password, :user
 
   validates :email, presence: true
   validate :check_email, if: :email
@@ -8,12 +8,20 @@ class Session
 
   validates_format_of :email, with: /.+@.+/, if: :email
   validates :password, length: {minimum: 6}, if: :password
+  validate :email_password_match, if: Proc.new { |s| s.email.present? and s.password.present? }
 
 
   def check_email
-    user = User.find_by_email email
+    user ||= User.find_by_email email
     if user.nil?
       errors.add :email, :not_found
+    end
+  end
+
+  def email_password_match
+    user ||= User.find_by_email email
+    if user and not user.authenticate(password) 
+      errors.add :password, :mismatch
     end
   end
 
