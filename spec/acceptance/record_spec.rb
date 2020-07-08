acf87c14084927cd68ebd41660832094816f6356
frankfang <frankfang1990@gmail.com>
2020-07-08 17:37:38 +0800

    更新 record 的文档搞定

--- spec/acceptance/record_spec.rb
+++ spec/acceptance/record_spec.rb
@@ -4,13 +4,16 @@ require 'rspec_api_documentation/dsl'
 resource "Records" do
   let(:record) { Record.create! amount: 10000, category: 'income' }
   let(:id) { record.id }
+  let(:amount) {10000}
+  let(:category) {'income'}
+  let(:notes){'中奖'}
   post "/records" do
     parameter :amount, '金额', type: :integer, required: true
     parameter :category, '类型:1 outgoings|2 income', type: :string, required: true
     parameter :notes, '备注', type: :string
     example "创建记录" do
       sign_in
-      do_request(amount: 10000, category: 'income', notes: '彩票中奖')
+      do_request
       expect(status).to eq 200
     end
   end
@@ -42,6 +45,17 @@ resource "Records" do
     example '获取一个记录' do
       sign_in
       do_request
+      expect(status).to eq 200
+    end
+  end
+
+  patch "/records/:id" do
+    parameter :amount, '金额', type: :integer
+    parameter :category, '类型:1 outgoings|2 income'
+    parameter :notes, '备注', type: :string
+    example '更新一个记录' do
+      sign_in
+      do_request amount: 9900
       expect(status).to eq 200
     end
   end
