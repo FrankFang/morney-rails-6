3d746fa2cc42b65eeadba855745fd308d3ec0eac
frankfang <frankfang1990@gmail.com>
2020-07-08 17:24:54 +0800

    获取 records / record 的文档搞定

--- spec/acceptance/record_spec.rb
+++ spec/acceptance/record_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "Records" do
+  let(:record) { Record.create! amount: 10000, category: 'income' }
+  let(:id) { record.id }
   post "/records" do
     parameter :amount, '金额', type: :integer, required: true
     parameter :category, '类型:1 outgoings|2 income', type: :string, required: true
@@ -14,9 +16,30 @@ resource "Records" do
   end
 
   delete "/records/:id" do
-    let(:record){ Record.create! amount: 10000, category: 'income'}
-    let(:id) {record.id}
     example "删除记录" do
+      sign_in
+      do_request
+      expect(status).to eq 200
+    end
+  end
+
+  get "/records" do
+    parameter :page, '页码', type: :integer
+
+    let(:page) { 1 }
+
+    (1..11).to_a.map do
+      Record.create! amount: 10000, category: 'income'
+    end
+    example '获取所有记录' do
+      sign_in
+      do_request
+      expect(status).to eq 200
+    end
+  end
+
+  get "/records/:id" do
+    example '获取一个记录' do
       sign_in
       do_request
       expect(status).to eq 200
